2a0058f2b521064aea59733d51cab28b5b2c0d47
Dylan Socolobsky <dsocolobsky@gmail.com>
2024-09-22 17:18:46 -0300

    Simple TCP listener

--- main.go
+++ main.go
@@ -2,8 +2,38 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Received: %s\n", buffer[:n])
+	}
+}
+
+func runServer() {
+	server, err := net.Listen("tcp", "localhost:6379")
+	if err != nil {
+		panic(err)
+	}
+	defer server.Close()
+
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go handleConn(conn)
+	}
+}
+
 func main() {
-	fmt.Println("Reddys")
+	runServer()
 }
