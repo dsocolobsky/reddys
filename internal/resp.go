84eeda9c72d08db1ba5b0d90ff152ec8ea322e40
Dylan Socolobsky <dsocolobsky@gmail.com>
2024-09-24 15:31:22 -0300

    Read arrays and fix bulk string reading

--- internal/resp.go
+++ internal/resp.go
@@ -22,7 +22,7 @@ func CraftBulkString(message string) string {
 }
 
 // ReadBulkString reads a RESP bulk string of the type "$length\r\nmessage\r\n" into a string "message"
-func ReadBulkString(message string) string {
+func ReadBulkString(message string) (string, int) {
 	if message[0] != '$' {
 		panic("Invalid bulk string")
 	}
@@ -31,12 +31,77 @@ func ReadBulkString(message string) string {
 	if lineBreakIdx == -1 {
 		panic("Invalid bulk string")
 	}
-	length := message[:lineBreakIdx]
+	lengthStr := message[:lineBreakIdx]
 	message = message[lineBreakIdx+2:]
-	lengthInt := 0
-	fmt.Sscanf(length, "%d", &lengthInt)
-	if len(message) < lengthInt {
+	length := 0
+	fmt.Sscanf(lengthStr, "%d", &length)
+	if len(message) < length {
 		panic("Invalid bulk string")
 	}
-	return message[:lengthInt]
+	message = message[:length]
+	// Add 4 for both pairs of \r\n, one for $ and len(lengthStr) for the number in the length
+	return message[:length], length + 4 + 1 + len(lengthStr)
+}
+
+func ReadArray(message string) []string {
+	fmt.Println("Reading array: ", message)
+	if message[0] != '*' {
+		panic("Invalid array")
+	}
+	message = message[1:]
+	firstLineBreakIdx := strings.Index(message, "\r\n")
+	if firstLineBreakIdx == -1 {
+		panic("Invalid array")
+	}
+	lengthStr := message[:firstLineBreakIdx]
+	message = message[firstLineBreakIdx+2:]
+	length := 0
+	fmt.Sscanf(lengthStr, "%d", &length)
+	fmt.Println("Array length: ", length)
+	array := make([]string, length)
+	arrayIdx := 0
+	for len(message) > 0 {
+		fmt.Println("msg: ", message)
+		msg, read := ReadRESP(message)
+		array[arrayIdx] = msg
+		message = message[read:]
+		arrayIdx++
+	}
+	return array
+}
+
+func ReadSimpleString(message string) (string, int) {
+	return readSimple(message, "+")
+}
+
+func ReadSimpleError(message string) (string, int) {
+	return readSimple(message, "-")
+}
+
+func readSimple(message string, ch string) (string, int) {
+	if message[0] != ch[0] {
+		panic("Invalid simple string")
+	}
+	message = message[1:]
+	// Here we stop at the first \r\n and ignore everything that follows, might be wrong.
+	splitted := strings.SplitN(message, "\r\n", 2)
+	return splitted[0], len(splitted[0]) + 3 // Add 3 to account for the + and \r\n
+}
+
+func ReadRESP(message string) (string, int) {
+	message = strings.TrimSpace(message)
+	switch message[0] {
+	case '+':
+		return ReadSimpleString(message)
+	case '-':
+		return ReadSimpleError(message)
+	case '$':
+		return ReadBulkString(message)
+	case '*':
+		// Not yet implemented
+		//return ReadArray(message)
+	default:
+		panic("Invalid RESP message")
+	}
+	return "", -1
 }
--- internal/resp_test.go
+++ internal/resp_test.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -38,16 +39,72 @@ func TestCraftBulkStringNull(t *testing.T) {
 
 func TestReadBulkString(t *testing.T) {
 	expected := "message"
-	actual := ReadBulkString("$7\r\nmessage\r\n")
+	raw := "$7\r\nmessage\r\n"
+	actual, read := ReadBulkString(raw)
 	if expected != actual {
 		t.Errorf("Expected %s but got %s", expected, actual)
+	}
+	if read != len(raw) {
+		t.Errorf("Expected to read %d but got %d", len(raw), read)
 	}
 }
 
 func TestReadBulkStringMultipleLines(t *testing.T) {
 	expected := "some\nother\r\nmessage"
-	actual := ReadBulkString("$19\r\nsome\nother\r\nmessage\r\n")
+	raw := "$19\r\nsome\nother\r\nmessage\r\n"
+	actual, read := ReadBulkString(raw)
 	if expected != actual {
 		t.Errorf("Expected %s but got %s", expected, actual)
+	}
+	if read != len(raw) {
+		t.Errorf("Expected to read %d but got %d", len(raw), read)
+	}
+}
+
+func TestReadSimpleString(t *testing.T) {
+	expected := "message"
+	raw := "+message\r\n"
+	actual, read := ReadSimpleString(raw)
+	if expected != actual {
+		t.Errorf("Expected %s but got %s", expected, actual)
+	}
+	if read != len(raw) {
+		t.Errorf("Expected to read %d but got %d", len(raw), read)
+	}
+}
+
+func TestReadSimpleError(t *testing.T) {
+	expected := "message"
+	raw := "-message\r\n"
+	actual, read := ReadSimpleError(raw)
+	if expected != actual {
+		t.Errorf("Expected %s but got %s", expected, actual)
+	}
+	if read != len(raw) {
+		t.Errorf("Expected to read %d but got %d", len(raw), read)
+	}
+}
+
+func TestReadArrayOfSimpleStrings(t *testing.T) {
+	expected := []string{"one", "two", "three"}
+	actual := ReadArray("*3\r\n+one\r\n+two\r\n+three\r\n")
+	fmt.Println(expected)
+	fmt.Println(actual)
+	for i, v := range expected {
+		if v != actual[i] {
+			t.Errorf("Expected %s but got %s", v, actual[i])
+		}
+	}
+}
+
+func TestReadArrayOfBulkStrings(t *testing.T) {
+	expected := []string{"one", "two", "three"}
+	actual := ReadArray("*3\r\n$3\r\none\r\n$3\r\ntwo\r\n$5\r\nthree\r\n")
+	fmt.Println(expected)
+	fmt.Println(actual)
+	for i, v := range expected {
+		if v != actual[i] {
+			t.Errorf("Expected %s but got %s", v, actual[i])
+		}
 	}
 }
